3ff7dddeb4a7686f6a2343e4f761c6c0feedb8ab
linli <linli@glanway.com>
2018-09-07 17:05:08 +0800

    Borrowperson.java

---
+++ src/main/java/com/leo/model/Borrowperson.java
@@ -0,0 +1,57 @@
+package com.leo.model;
+
+import java.util.Date;
+
+public class Borrowperson {
+String name;
+String classes;
+String bookname;
+Date borrowdate;
+String id;
+
+public Borrowperson(){
+	this.id = java.util.UUID.randomUUID().toString();
+	this.borrowdate = new Date();
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getClasses() {
+	return classes;
+}
+
+public void setClasses(String classes) {
+	this.classes = classes;
+}
+
+public String getBookname() {
+	return bookname;
+}
+
+public void setBookname(String bookname) {
+	this.bookname = bookname;
+}
+
+public Date getBorrowdate() {
+	return borrowdate;
+}
+
+public void setBorrowdate(Date borrowdate) {
+	this.borrowdate = borrowdate;
+}
+
+public String getId() {
+	return id;
+}
+
+public void setId(String id) {
+	this.id = id;
+}
+
+}
